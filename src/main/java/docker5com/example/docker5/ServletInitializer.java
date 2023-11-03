5f8307c1038bf7274976ffc6e7ce423bee1e5374
Nachiket-01 <nachiketshinde2001@gmail.com>
2023-11-03 09:19:34 +0530

    uploading

---
+++ src/main/java/docker5com/example/docker5/ServletInitializer.java
@@ -0,0 +1,13 @@
+package docker5com.example.docker5;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Docker5Application.class);
+	}
+
+}
---
+++ src/test/java/docker5com/example/docker5/Docker5ApplicationTests.java
@@ -0,0 +1,13 @@
+package docker5com.example.docker5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Docker5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
